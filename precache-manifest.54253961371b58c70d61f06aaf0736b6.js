13bf9d97f40772d3135ab995f672330fa6f3a2fc
CalebVincent <vincent_806@yahoo.com>
2020-03-05 05:26:34 -0600

    Updates

---
+++ precache-manifest.54253961371b58c70d61f06aaf0736b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21d93219a3be958e704a9377f42f2a4",
+    "url": "/updatedportfolio-2/index.html"
+  },
+  {
+    "revision": "221281a532506cad372d",
+    "url": "/updatedportfolio-2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e541471ec7bc8328e693",
+    "url": "/updatedportfolio-2/static/js/2.fbf8753b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/updatedportfolio-2/static/js/2.fbf8753b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221281a532506cad372d",
+    "url": "/updatedportfolio-2/static/js/main.81995b05.chunk.js"
+  },
+  {
+    "revision": "9030c727d083b97fc0eb",
+    "url": "/updatedportfolio-2/static/js/runtime-main.c284fc4a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/updatedportfolio-2/static/media/logo.5d5d9eef.svg"
+  }
+]);
